b4fdd0c3030c9863584cef9a3e8263cd5316ed76
sujal-pawar <sujalpawar00007@gmail.com>
2025-06-18 15:51:44 +0530

    search-startup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
-    <div className="bg-black">
-      <h1 className="heading">Home</h1>
-    </div>
+    <section className="pink_container">
+      <h1 className="heading">Pitch Your StartUp <br /> Connect with Entrepreneurs</h1>
+      <p className="sub-heading !max-w-3xl">
+        Join our community to share your vision and collaborate on innovative projects.
+      </p>
+      <SearchForm/>
+    </section>
   );
 }
